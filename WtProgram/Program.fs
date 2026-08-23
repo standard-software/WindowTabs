446c76565ebb9509f1c16e93ae9491884a0fb3e3
Satoshi Yamamoto <standard.software.net@gmail.com>
2026-08-23 13:31:48 +0900

    next version

--- WtProgram/Program.fs
+++ WtProgram/Program.fs
@@ -89,7 +89,7 @@ type ClosedTabInfo = {
 let normalizeClosedTabTitle (t: string) = t.Replace("● ", "")
 
 type Program() as this =
-    let version = "ss_2026.08.21"
+    let version = "ss_2026.08.21_next1"
     let isStandAlone = System.Diagnostics.Debugger.IsAttached
 
     let Cell = CellScope()
