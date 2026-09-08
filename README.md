17c696f8dc8266bb939228f60393b3dc2b020c60
Satoshi Yamamoto <standard.software.net@gmail.com>
2026-09-09 05:27:00 +0900

    next version ss_2026.09.08_next8

--- README.md
+++ README.md
@@ -117,7 +117,7 @@ Mr./Ms. leafOfTree also created a fork with various improvements:
 
 ## Version
 
-Latest version: **ss_2026.09.08_next8_build14**
+Latest version: **ss_2026.09.08_next8**
 
 See [version.md](version.md) for details.
 
--- WtProgram/Program.fs
+++ WtProgram/Program.fs
@@ -234,7 +234,7 @@ module RestoreTrace =
 #endif
 
 type Program() as this =
-    let version = "ss_2026.09.08_next8_build14"
+    let version = "ss_2026.09.08_next8"
     let isStandAlone = System.Diagnostics.Debugger.IsAttached
 
     let Cell = CellScope()
